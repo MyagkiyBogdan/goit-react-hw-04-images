5f109766d4c0580096749ea1d20ec9bafb136b1c
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-06-11 00:44:38 +0300

    idk

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
-// import { Circles } from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
 import styles from './Loader.module.css';
 
 function Loader() {
   return (
     <div className={styles.loader}>
-      {/* <Circles height="35" width="35" color="black" ariaLabel="loading" /> */}
+      <Circles height="35" width="35" color="black" ariaLabel="loading" />
       <span className={styles.text}>Loading...</span>
     </div>
   );
